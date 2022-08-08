ca6d8402f1847e14d30ed8daacaddafba1e99b4a
Bryant Pham <bryant.pham@cdk.com>
2022-08-08 08:09:36 -0700

    added new functions

--- index.js
+++ index.js
@@ -1,32 +1,97 @@
 // canvas related vars
-var canvas=document.createElement("canvas");
+var canvas=document.getElementById("game");
 var ctx=canvas.getContext("2d");
-canvas.width=window.outerWidth;
+canvas.width=(8/10)*window.outerWidth;
 canvas.height=window.innerHeight;
 var cw=canvas.width;
 var ch=canvas.height;
-document.body.appendChild(canvas);
-canvas.style.border='1px solid red';
+
+var clientWidth = document.getElementById('game').clientWidth;
+var clientHeight = document.getElementById('game').clientHeight;
+
+var score = document.getElementById("score");
+
+
+//variables to keep track of attempts
+var successfulTries=0;
+var failedTries=0;
+
+
+// save relevant information about shapes drawn on the canvas
+var shapes=[];
+var dontDraw=[];
+
+
+// drag related vars
+var isDragging=false;
+var itemDraggging;
+var startX,startY;
+var initialClickX;
+var initialClickY;
+
+// hold the index of the shape being dragged (if any)
+var selectedShapeIndex;
+
+// load the image
+
+var trash = [];
+var recycle = [];
+var compost = []
+
+var trash1= new Image();
+trash1.src='./img/trash/trash1.png'
+var trash2= new Image();
+trash2.src='./img/trash/trash2.png'
+var trash3= new Image();
+trash3.src='./img/trash/trash3.png'
+var trash4= new Image();
+trash4.src='./img/trash/trash4.png'
+var trash5= new Image();
+trash5.src='./img/trash/trash5.png'
+
+var compost1 = new Image();
+compost1.src='./img/compost/compost1.png'
+var compost2 = new Image();
+compost2.src='./img/compost/compost2.png'
+var compost3 = new Image();
+compost3.src='./img/compost/compost3.png'
+var compost4 = new Image();
+compost4.src='./img/compost/compost4.png'
+var compost5 = new Image();
+compost5.src='./img/compost/compost5.png'
+
+var recycle1 = new Image();
+recycle1.src='./img/recycle/recycle1.png'
+var recycle2 = new Image();
+recycle2.src='./img/recycle/recycle2.png'
+var recycle3 = new Image();
+recycle3.src='./img/recycle/recycle3.png'
+var recycle4 = new Image();
+recycle4.src='./img/recycle/recycle4.png'
+var recycle5 = new Image();
+recycle5.src='./img/recycle/recycle5.png'
+
+trash.push(trash1,trash2,trash3,trash4,trash5);
+compost.push(compost1,compost2,compost3,compost4,compost5);
+recycle.push(recycle1,recycle2,recycle3,recycle4,recycle5);
 
 
 
-var apple= new Image();
-apple.src='./img/apple.png'
 
-var bag = new Image();
-bag.src='./img/bag.png'
-
-var paper = new Image();
-paper.src='./img/paper.png'
+var background = new Image();
+background.src='./img/background.png'
 
 
+var bin1 = new Image();
+var bin2 = new Image();
+var bin3 = new Image();
+
+bin1.src='./img/bin1.png'
+bin2.src='./img/bin2.png'
+bin3.src='./img/bin3.png'
 
 
 
-
-//Create array of trash 
-var trashArray = [];
-trashArray.push(apple,bag,paper);
 
 //Randomize trash
 
@@ -47,24 +112,14 @@ window.onscroll=function(e){ reOffset(); }
 window.onresize=function(e){ reOffset(); }
 canvas.onresize=function(e){ reOffset(); }
 
-// save relevant information about shapes drawn on the canvas
-var shapes=[];
 
-// drag related vars
-var isDragging=false;
-var startX,startY;
-
-// hold the index of the shape being dragged (if any)
-var selectedShapeIndex;
-
-// load the image
-
-
-function handleLoad(imageObj,xVal,yVal,widthVal,heightVal)
+function handleLoad(imageObj,xVal,yVal,widthVal,heightVal,trash)
 {
     console.log("behing handled");
     console.log(imageObj);
-    shapes.push( {x:xVal, y:yVal, width:widthVal, height:heightVal , image:imageObj});
+    shapes.push( {x:xVal, y:yVal, width:widthVal, height:heightVal , image:imageObj, type:trash});
+    //    shapes.push( {x:xVal, y:yVal, width:widthVal, height:heightVal , image:imageObj,type:trash});
+
     drawAll();
     canvas.onmousedown=handleMouseDown;
     canvas.onmousemove=handleMouseMove;
@@ -74,54 +129,32 @@ function handleLoad(imageObj,xVal,yVal,widthVal,heightVal)
 }
 
 
-function loadAllTrash(trash)
+function loadAllTrash(trashArray,trashType)
 {
 
-
-    
-
-    for(let i=0;i<10;i++)
+    for(let i=0;i<5;i++)
     {
         var spawnX=Math.random() * ((this.cw-150) - 0) + 0;
         var spawnY=Math.random() * ((this.ch-500) - 0) + 0;
-        var randomTrash= Math.floor(Math.random() * 3);
+        console.log(spawnX,spawnY);
+
+        var randomTrash= Math.floor(Math.random() * 5);
 
         console.log(randomTrash);
-        handleLoad(trash[randomTrash],spawnX,spawnY,150,150);
+        handleLoad(trashArray[randomTrash],spawnX,spawnY,150,150,trashType);
         spawnX+=200;
     }
+
+ 
 }
 
 window.onload= function(){
-    loadAllTrash(trashArray);
-}
-//loadAllTrash(trashArray);
+    loadAllTrash(compost,"compost");
+    loadAllTrash(recycle,"recycle");
 
+    loadAllTrash(trash,"trash");
 
-// apple.onload=function(){
-//     // define one image and save it in the shapes[] array
-//     shapes.push( {x:0, y:0, width:150, height:150, image:apple} );
-//     // draw the shapes on the canvas
-//     drawAll();
-//     // listen for mouse events
-//     canvas.onmousedown=handleMouseDown;
-//     canvas.onmousemove=handleMouseMove;
-//     canvas.onmouseup=handleMouseUp;
-//     canvas.onmouseout=handleMouseOut;
-// };
-
-// bag.onload=function(){
-//     // define one image and save it in the shapes[] array
-//     shapes.push( {x:150, y:0, width:150, height:150, image:bag} );
-//     // draw the shapes on the canvas
-//     drawAll();
-//     // listen for mouse events
-//     canvas.onmousedown=handleMouseDown;
-//     canvas.onmousemove=handleMouseMove;
-//     canvas.onmouseup=handleMouseUp;
-//     canvas.onmouseout=handleMouseOut;
-// };
-// put your image src here!
+}// put your image src here!
 
 
 // given mouse X & Y (mx & my) and shape object
@@ -150,10 +183,14 @@ function handleMouseDown(e){
     // calculate the current mouse position
     startX=parseInt(e.clientX-offsetX);
     startY=parseInt(e.clientY-offsetY);
+    console.log(startX);
     // test mouse position against all shapes
     // post result if mouse is in a shape
     for(var i=0;i<shapes.length;i++){
         if(isMouseInShape(startX,startY,shapes[i])){
+            initialClickX=shapes[i].x;
+            initialClickY=shapes[i].y;
+            
             // the mouse is inside this shape
             // select this shape
             selectedShapeIndex=i;
@@ -174,6 +211,71 @@ function handleMouseUp(e){
     e.stopPropagation();
     // the drag is over -- clear the isDragging flag
     isDragging=false;
+
+    mouseX=parseInt(e.clientX-offsetX);
+    mouseY=parseInt(e.clientY-offsetY);
+
+    //trash bin
+    if(mouseX>0 && mouseX<0+300 && mouseY>(3/5)*clientHeight && mouseY<(3/5)*clientHeight+300 && shapes[selectedShapeIndex].type=="trash")
+    {
+        console.log("successful trash categorization");
+        successfulTries++;
+        dontDraw.push(selectedShapeIndex);
+        drawAll();
+        
+
+
+    }
+    else if(mouseX>(1/3)*clientWidth && mouseX<(1/3)*clientWidth+300 && mouseY>(3/5)*clientHeight && mouseY<(3/5)*clientHeight+300 && shapes[selectedShapeIndex].type=="recycle")
+    {
+        console.log("successful recycling categorization");
+        successfulTries++;
+        dontDraw.push(selectedShapeIndex);
+        drawAll();
+
+    }
+    else if(mouseX>(2/3)*clientWidth && mouseX<(2/3)*clientWidth+300 && mouseY>(3/5)*clientHeight && mouseY<(3/5)*clientHeight+300 && shapes[selectedShapeIndex].type=="compost")
+    {
+        console.log("successful composting categorization");
+        successfulTries++;
+        dontDraw.push(selectedShapeIndex);
+        drawAll();
+
+    }
+    else if(mouseX>0 && mouseX<0+300 && mouseY>(3/5)*clientHeight && mouseY<(3/5)*clientHeight+300 && shapes[selectedShapeIndex].type!="trash")
+    {
+        console.log("failed trash categorization");
+        failedTries++;
+        var selectedShape=shapes[selectedShapeIndex];
+        selectedShape.x=initialClickX;
+        selectedShape.y=initialClickY;
+        drawAll();
+
+    }
+    else if(mouseX>(1/3)*clientWidth && mouseX<(1/3)*clientWidth+300 && mouseY>(3/5)*clientHeight && mouseY<(3/5)*clientHeight+300 && shapes[selectedShapeIndex].type!="recycle")
+    {
+        console.log("failed recycling categorization");
+        failedTries++;
+        var selectedShape=shapes[selectedShapeIndex];
+        selectedShape.x=initialClickX;
+        selectedShape.y=initialClickY;
+        drawAll();
+
+    }
+    else if(mouseX>(2/3)*clientWidth && mouseX<(2/3)*clientWidth+300 && mouseY>(3/5)*clientHeight && mouseY<(3/5)*clientHeight+300 && shapes[selectedShapeIndex].type!="compost")
+    {
+        console.log("failed compost categorization");
+        failedTries++;
+        var selectedShape=shapes[selectedShapeIndex];
+        selectedShape.x=initialClickX;
+        selectedShape.y=initialClickY;
+        drawAll();
+
+    }
+    console.log(successfulTries);
+
+
+
 }
 
 function handleMouseOut(e){
@@ -213,33 +315,73 @@ function handleMouseMove(e){
 // redraw all shapes in their current positions
 function drawAll(){
     ctx.clearRect(0,0,cw,ch);
+    if(successfulTries==5)
+    {
+        document.getElementById("earth1").src='./img/earth5.png';
+
+    }
+    ctx.drawImage(background,0,0,clientWidth,clientHeight);
+    drawBin();
     for(var i=0;i<shapes.length;i++){
-        var shape=shapes[i];
-        if(shape.image){
-            // it's an image
-            ctx.drawImage(shape.image,shape.x,shape.y,150,150);
-            drawBin();
+
+        if(dontDraw.includes(i))
+        {
+        }
+        else{
+            var shape=shapes[i];
+
+            if(shape.image){
+                // it's an image
+                ctx.drawImage(shape.image,shape.x,shape.y,150,150);
+            }
         }
     }
 }
   //Draw temporary bins 
   function drawBin(amount)
   {
-    console.log("entered");
-    ctx.fillStyle="#000000";
-    ctx.beginPath();
-    ctx.arc(400, 700, 40, 0, Math.PI * 2);
-    ctx.fill();
 
+    ctx.drawImage(bin1,0,(3/5)*clientHeight,300,300);
+    ctx.drawImage(bin2,(1/3)*clientWidth,(3/5)*clientHeight,300,300);
+    ctx.drawImage(bin3,(2/3)*clientWidth,(3/5)*clientHeight,300,300);
     
-    ctx.beginPath();
-    ctx.arc(900, 700, 40, 0, Math.PI * 2);
-    ctx.fill();
-
-    ctx.beginPath();
-    ctx.arc(1400, 700, 40, 0, Math.PI * 2);
-    ctx.fill();
   }
 
   drawBin(3);
 
+
+  //Need to finish 
+  //Add music to game
+  function sound(src) {
+    this.sound = document.createElement("audio");
+    this.sound.src = src;
+    this.sound.setAttribute("preload", "auto");
+    this.sound.setAttribute("controls", "none");
+    this.sound.style.display = "none";
+    document.body.appendChild(this.sound);
+    this.play = function(){
+      this.sound.play();
+    }
+    this.stop = function(){
+      this.sound.pause();
+    }
+  }
+  
+var music=0;
+var mySound = new sound("music.wav");
+
+function turnMusicOn()
+{
+    console.log("entered");
+    if(music==0)
+    {
+        console.log("turn music on");
+        mySound.play();
+        music=1;
+    }
+    else{
+        mySound.pause();
+        music=0;
+    }
+
+}
